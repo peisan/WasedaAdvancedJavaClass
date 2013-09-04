9b75834716bd7b37149ec2f178aed9626198b5a6
Ippei Arita <ippei@Ippeis-MacBook-Pro.local>
2013-09-04 16:02:51 +0900

    fix Actor classes

--- src/main/scala/lesson/scala/thread/PrintIntActorReact.scala
+++ src/main/scala/lesson/scala/thread/PrintIntActorReact.scala
@@ -2,18 +2,18 @@ package lesson.scala.thread
 
 import scala.actors.Actor
 
-class PrintIntActorReact extends Actor{
+class PrintIntActorReact extends Actor {
   def act(): Unit = {
-    loop {  // continue receive without restart
-      react {
-        case i: Int => reply("reply" + i.toString)
-        case _ => reply("Not Supported Type")
-      }
+    //loop {  // continue receive without restart
+    react {
+      case i: Int => reply("reply" + i.toString)
+      case _ => reply("Not Supported Type")
     }
+    //}
   }
 }
 
-object PrintIntActorReact{
+object PrintIntActorReact {
   def main(args: Array[String]): Unit = {
     println("main thread start")
     (1 to 10).foreach(
--- src/main/scala/lesson/scala/thread/PrintIntActorReceive.scala
+++ src/main/scala/lesson/scala/thread/PrintIntActorReceive.scala
@@ -4,23 +4,23 @@ import scala.actors.Actor
 
 class PrintIntActorReceive extends Actor {
   def act(): Unit = {
-    loop {  // continue receive without restart
-      val number :String = receive {
-        case i: Int => i.toString
-        case _ => "Not Supported Type"
-      }
-      println("num " + number)
+    //loop {  // continue receive without restart
+    val number: String = receive {
+      case i: Int => i.toString
+      case _ => "Not Supported Type"
     }
+    println("num " + number)
+    //}
   }
 }
 
-object PrintIntActorReceive{
+object PrintIntActorReceive {
   def main(args: Array[String]): Unit = {
     println("main thread start")
     (1 to 10).foreach(
       i => {
         println("creating Actor: " + i)
-        println(new PrintIntActorReceive().start() ! i)
+        new PrintIntActorReceive().start() ! i
       }
     )
     println("main thread end")
